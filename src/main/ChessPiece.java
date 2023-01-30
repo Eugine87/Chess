695d99b5286969e064c35835670e3a63bb876ce8
Eugine87 <amelkovevgenij@gmail.com>
2023-01-30 19:13:09 +0300

    Yeeeh

---
+++ src/main/ChessPiece.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Star
+ */
+public abstract class ChessPiece {
+    String color;
+    boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    public abstract String getColor();
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+    public abstract String getSymbol();
+}
